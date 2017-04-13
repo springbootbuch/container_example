0f293908cb9fc8d3b2e6269673c9d44436151260
Michael J. Simons <michael@simons.ac>
2017-04-13 21:24:05 +0200

    Fix resource path

--- src/main/java/de/springbootbuch/container_example/Application.java
+++ src/main/java/de/springbootbuch/container_example/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("beans.xml")
+@ImportResource("classpath:/beans.xml")
 public class Application {
 
     public static void main(String... args) {
